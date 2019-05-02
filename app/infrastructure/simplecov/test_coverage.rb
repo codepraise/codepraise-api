be4ec7069750c1704236c7492f98916b67e652c4
vicxu <xumingyo@gmail.com>
2019-05-02 18:19:15 +0800

    finish test coverage class for reporting test coverage and its unit test

---
+++ app/infrastructure/simplecov/test_coverage.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'date'
+
+module CodePraise
+  module SimpleCov
+    # Report the test coverage by a file
+    class TestCoverage
+      COVERAGE_PATH = '/coverage/.resultset.json'
+
+      def initialize(repo_path)
+        @coverage_hash = JSON.parse(File.read(repo_path + COVERAGE_PATH))
+      end
+
+      def coverage_hash
+        @coverage_hash['RSpec']['coverage']
+      end
+
+      def coverage_report(file_path)
+        {
+          coverage: test_coverage(file_path),
+          datetime: datetime
+        }
+      end
+
+      def datetime
+        @datetime ||= Time.strptime(timestamp, '%s')
+      end
+
+      def timestamp
+        @coverage_hash['RSpec']['timestamp'].to_s
+      end
+
+      def project_path
+        @project_path ||= longest_repeating_substring(coverage_hash.keys)
+      end
+
+      def test_array(file_path)
+        path = project_path + file_path
+        coverage_hash[path]
+      end
+
+      def test_coverage(file_path)
+        calculate_test_coverage(test_array(file_path))
+      end
+
+      private
+
+      def longest_repeating_substring(string_array)
+        result = ''
+        char_len = string_array[0].length
+        char_len.times do |index|
+          char_array = string_array.map { |string| string[index] }
+          char_set = Set.new(char_array)
+          result += char_set.first if char_set.length == 1
+          break if char_set.length > 1
+        end
+        result
+      end
+
+      def calculate_test_coverage(coverage_array)
+        return 0 if coverage_array.nil? || coverage_array.empty?
+
+        total_lines = coverage_array.reject(&:nil?)
+        cover_lines = total_lines.reject(&:zero?)
+        cover_lines.length.to_f / total_lines.length
+      end
+    end
+  end
+end
