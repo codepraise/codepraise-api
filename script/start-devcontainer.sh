375d7f7a2188b531a80eee1e13e0c671f2793367
tienyulin <t.tienyulin@gmail.com>
2022-10-07 22:56:51 +0000

    test

--- script/start-devcontainer.sh
+++ script/start-devcontainer.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # Start the devcontainer
 
-if [ "${SHELL}" = "/bin/bash" ]; then
-  source ~/.bashrc
-elif [ "${SHELL}" = "/bin/zsh" ]; then
-  source ~/.zshrc
+project_root=$(dirname "${BASH_SOURCE[0]}")/..
+if [ -e ${project_root}/.env ]; then
+  source ${project_root}/.env
 fi
+
+export COMOPSE_FILE=${COMPOSE_FILE:-"${project_root}/.devcontainer/docker-compose.yml"}
+export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-"codepraise"}
+
+#if [ "${SHELL}" = "/bin/bash" ]; then
+#  source ~/.bashrc
+#elif [ "${SHELL}" = "/bin/zsh" ]; then
+#  source ~/.zshrc
+#fi
 
 docker-compose up -d --build && docker-compose exec api /bin/bash -l
